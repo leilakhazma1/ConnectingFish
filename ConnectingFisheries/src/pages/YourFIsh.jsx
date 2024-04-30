930bb4e579640afc718f57c3a7d609a97765accc
leilakhazma1 <leilakhazma7@gmail.com>
2024-04-30 10:16:55 +1000

    StoreLocator

--- ConnectingFisheries/src/pages/YourFIsh.jsx
+++ ConnectingFisheries/src/pages/YourFIsh.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css'
+import StoreLocator from '../components/StoreLocator';
 
 
 
@@ -7,7 +8,7 @@ function YourFish() {
   return (
     <div className="About">
       
-         
+         <StoreLocator />
       </div>
  
       
